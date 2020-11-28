b46988d654db10620ef013f4f23934af43d645fa
imt <sukachev@pm.me>
2020-11-28 16:52:47 +0000

    add unit, unitaicontroller, unitability

---
+++ Assets/Scripts/Unit/UnitAIController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitAIController : MonoBehaviour
+{
+    public Unit unit;
+    public NavMeshAgent agent;
+
+    Unit FindClosestUnit(UnitTeam team)
+    {
+        Unit[] units = GameObject.FindObjectsOfType<Unit>();
+        float closestDistance = 0;
+        Unit closestUnit = null;
+        foreach (Unit u in units)
+        {
+            if (u.team != team)
+                continue;
+            float distance = Vector3.Distance(u.transform.position, unit.transform.position);
+            if (!closestUnit || distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestUnit = u;
+            }
+        }
+        return closestUnit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TODO select best ability first
+
+        Unit target = FindClosestUnit(UnitTeam.Player);
+        float distance = Vector3.Distance(target.transform.position, unit.transform.position);
+
+        if (target && distance > 1.5f) // TODO do some ability checks
+        {
+            Debug.Log(distance);
+            agent.isStopped = false;
+            agent.SetDestination(target.transform.position);
+        }
+        else
+        {
+            agent.isStopped = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Unit/UnitAbility.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitAbility : MonoBehaviour
+{
+    public float cooldownTime = 1000.0f;
+    private float lastUsedTime;
+
+    // range in meters
+    public float range = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void Fire()
+    {
+        lastUsedTime = Time.time;
+    }
+
+    bool CanFire()
+    {
+        Unit[] units = GameObject.FindObjectsOfType<Unit>();
+        foreach (Unit unit in units) {
+            Debug.Log(unit);
+
+        }
+        return false;
+    }
+}
